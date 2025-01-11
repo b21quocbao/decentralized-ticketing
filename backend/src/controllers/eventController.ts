90b579e7c3dc6aa18b73813998ef517c3c16b59a
Gan Ze Yu <52880557+zeyugan@users.noreply.github.com>
2025-01-11 20:38:37 +0800

    create event and attach ticket lists to event

--- backend/src/controllers/eventController.ts
+++ backend/src/controllers/eventController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Event from '../models/Event';
+import Ticket from '../models/Ticket';
 
 export const listEvents = async (req: Request, res: Response) => {
   const events = await Event.find();
@@ -13,5 +14,18 @@ export const getEventDetail = async (req: Request, res: Response) => {
     res.status(404).json({ message: 'Event not found' });
     return;
   }
+
+  // get list of asscoiated tickets and attach to event
+  const tickets = await Ticket.find({ eventId: id });
+  event.set('tickets', tickets, { strict: false });
+
+  res.json(event);
+};
+
+// create a new event
+export const createEvent = async (req: Request, res: Response) => {
+  const { name, description, date, location } = req.body;
+  const event = new Event({ name, description, date, location });
+  await event.save();
   res.json(event);
 };
