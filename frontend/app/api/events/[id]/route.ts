7479e810414ab22d7234cdfe4f2262ff9a6df07c
Bao Ngo <ngobao2001@gmail.com>
2025-01-22 14:49:38 +0800

    try different query command

--- frontend/app/api/events/[id]/route.ts
+++ frontend/app/api/events/[id]/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request,
   { params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id
   const sql = neon(process.env.DATABASE_URL as string);
-  const [event] = await sql`SELECT * FROM events WHERE id = ${id}`;
+  const [event] = await sql("SELECT * FROM events WHERE id = $1", [id]);
 
   return Response.json({
     id: event.id,
